5c30bd81956a7ab6a72a6c59c653e8e0a5983532
Matias <ma77gmt@gmail.com>
2020-01-28 12:49:17 +0100

    .

---
+++ Boletin2 2Evaluacion/src/Persona.java
@@ -0,0 +1,21 @@
+/*Ejercicio1:
+Apartado 1: Crear la clase Persona con los campos necesarios para representar los valores de nombre,
+apellidos, fecha de nacimiento (ver clase java.util.Date para representarla) y estatura.
+Crear tres objetos de tipo Persona (en el main) con los siguientes datos:
+Jose Rodriguez Mata, 15/01/1965, 165cm
+María Sánchez Gómez 26/12/1980, 170cm
+Alejandro Gómez Gómez, 01/11/2001, 190cm
+Jose Manuel Pérez Pons, 01/05/2010, 127cm
+*/
+import java.time.LocalDate;
+
+public class Persona {
+
+	 	
+	 	public LocalDate nombre;
+	 	public LocalDate apellidos;
+	 	public LocalDate fnacimiento; 
+	 	public LocalDate estatura;
+	 	
+}
+	
